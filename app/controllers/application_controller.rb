6c978aeea0d5346f60316af264a6ae4c7f6d94a0
github username <github email address>
2019-03-01 19:04:58 -0400

    Added only logged in users can see logout in the navbar and only logged out users can see login and signup in the navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    flash.now[:danger] = "I'm in danger."
+    # flash.now[:danger] = "I'm in danger."
     erb :welcome
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
